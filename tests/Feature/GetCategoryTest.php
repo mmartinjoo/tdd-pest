abcf9f930b770b04a7b0ade3b22901826f48835d
Joó Martin <m4rt1n.j00@gmail.com>
2021-11-09 21:25:34 +0100

    Add GET /categories/category API

--- tests/Feature/GetCategoryTest.php
+++ tests/Feature/GetCategoryTest.php
@@ -2,7 +2,19 @@
 
 namespace Tests\Feature;
 
-class GetCategoryTest
-{
+use App\Models\Category;
+use function Pest\Laravel\getJson;
 
-}
+it('should return a category', function () {
+    $category = Category::factory([
+        'name' => 'Books',
+        'description' => 'Category for books'
+    ])->create();
+
+    $response = getJson(route('categories.show', ['category' => $category->id]))->json('data');
+    expect($response)->toMatchArray([
+        'id' => $category->id,
+        'name' => 'Books',
+        'description' => 'Category for books',
+    ]);
+});
